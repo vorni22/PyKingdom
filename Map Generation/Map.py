396fbacdb99c0b974a7b328af97794bf44d99e45
BadHeroDell <nicknameusername01@gmail.com>
2024-11-01 17:07:53 +0200

    Updated map class, now it no longer needs to be instantiated, instead the information inside it being stored statically

--- Map Generation/Map.py
+++ Map Generation/Map.py
@@ -1,16 +1,29 @@
 import Logic.Tile
 
-class Map:
-    def __init__(self, lines, columns):
-        self.lines = lines
-        self.columns = columns
-        self.tiles = []
-        self.init_tiles()
+# Holds information about the map. Should be generated once using init_map, then information should be accessed directly
+# from the class container via get_tile
+# @param lines: the number of lines the map has
+# @param columns: the number of columns the map has
+# @param tiles: 2d list of tiles. Tiles are generated using the init_tiles once, and then remained stored in the Map
+# class, which acts as a singleton container for all the map related information
 
-    def init_tiles(self):
+class Map:
+    lines = 0
+    columns = 0
+    tiles = []
+
+    @staticmethod
+    def init_map(lines, columns):
+        Map.lines = lines
+        Map.columns = columns
+        Map.init_tiles()
+
+    @staticmethod
+    def init_tiles():
         # TODO Add implementation for map tiles generation, type attribution, resource and feature distribution, to be
         #  done after the map heights distribution
         pass
 
-    def get_tile(self, line, column):
-        return self.tiles[line][column]
+    @staticmethod
+    def get_tile(line, column):
+        return Map.tiles[line][column]
