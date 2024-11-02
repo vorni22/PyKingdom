f2bcb438f50bd3d4aeac8783b331695923795341
BadHeroDell <nicknameusername01@gmail.com>
2024-11-02 13:15:48 +0200

    Added calculation for tile yields for cities

--- Logic/City.py
+++ Logic/City.py
@@ -17,11 +17,13 @@ city_center_buildings = ['Palace', 'Monument', 'Granary', 'Water Mill', 'Sewer']
 # @param buildings: list of buildings that have been built in this district. Buildings are specific to each district
 # and a building of tier 2 requires a tier 1 building to be built first
 class District:
-    def __init__(self, district_type):
+    def __init__(self, district_type, location_line, location_column):
         if district_type not in district_types:
             raise TypeError("Invalid district type")
         self.district_type = district_type
         self.buildings = []
+        self.location_line = location_line
+        self.location_column = location_column
 
     def add_building(self, building_id):
         if self.district_type == district_types[0]:
@@ -117,35 +119,50 @@ class District:
 class City:
     def __init__(self, city_name, center_line_location, center_column_location):
         self.city_name = city_name
-        self.districts = [District(district_types[7])]
+        self.districts = [District(district_types[7], center_line_location, center_column_location)]
         self.housing = 3
         self.population = 1
         self.tiles = [Map.Map.get_tile(center_line_location, center_column_location)]
-        self.init_tiles()
+        self.add_tiles(center_line_location, center_column_location)
         self.city_resources = Resources.CityResources(0, 0)
         self.city_resources_per_turn = Resources.CityResourcesPerTurn(0, 0)
+        self.resources_per_turn = Resources.ResourcesPerTurn(0, 0, 0)
         self.center_line_location = center_line_location
         self.center_column_location = center_column_location
 
-    def init_tiles(self):
-        self.tiles.append(Map.Map.get_tile(self.center_line_location, self.center_column_location - 1))
-        self.tiles.append(Map.Map.get_tile(self.center_line_location, self.center_column_location + 1))
-        self.tiles.append(Map.Map.get_tile(self.center_line_location + 1, self.center_column_location))
-        self.tiles.append(Map.Map.get_tile(self.center_line_location - 1, self.center_column_location))
-        if self.center_line_location % 2:
-            self.tiles.append(Map.Map.get_tile(self.center_line_location + 1, self.center_column_location - 1))
-            self.tiles.append(Map.Map.get_tile(self.center_line_location - 1, self.center_column_location - 1))
+    def add_tiles(self, line, column):
+        ref = Map.Map.get_tile(line, column - 1)
+        if ref not in self.tiles:
+            self.tiles.append(ref)
+        ref = Map.Map.get_tile(line, column + 1)
+        if ref not in self.tiles:
+            self.tiles.append(ref)
+        ref = Map.Map.get_tile(line + 1, column)
+        if ref not in self.tiles:
+            self.tiles.append(ref)
+        ref = Map.Map.get_tile(line - 1, column)
+        if ref not in self.tiles:
+            self.tiles.append(ref)
+        if line % 2:
+            ref = Map.Map.get_tile(line + 1, column - 1)
+            if ref not in self.tiles:
+                self.tiles.append(ref)
+            ref = Map.Map.get_tile(line - 1, column - 1)
+            if ref not in self.tiles:
+                self.tiles.append(ref)
         else:
-            self.tiles.append(Map.Map.get_tile(self.center_line_location + 1, self.center_column_location + 1))
-            self.tiles.append(Map.Map.get_tile(self.center_line_location - 1, self.center_column_location + 1))
+            ref = Map.Map.get_tile(line + 1, column + 1)
+            if ref not in self.tiles:
+                self.tiles.append(ref)
+            ref = Map.Map.get_tile(line - 1, column + 1)
+            if ref not in self.tiles:
+                self.tiles.append(ref)
 
-    def add_tile(self, line, column):
-        self.tiles.append(Map.Map.get_tile(line, column))
-
-    def add_district(self, district_name_id):
+    def add_district(self, district_name_id, location_line, location_column):
         if district_types[district_name_id] in self.districts and district_name_id != 5:
             raise ValueError("Can only build one district of this type in a city")
-        self.districts.append(District(district_types[district_name_id]))
+        self.districts.append(District(district_types[district_name_id], location_line, location_column))
+        self.add_tiles(location_line, location_column)
 
     def add_building(self, building_name_id, district_id):
         self.districts[district_id].add_building(building_name_id)
@@ -154,7 +171,6 @@ class City:
         total_resources_per_turn = Resources.ResourcesPerTurn(0, 0, 0)
         total_city_resources_per_turn = Resources.CityResourcesPerTurn(0, 0)
         self.housing = 3
-        has_aqueduct = False
         for district in self.districts:
             resources, city_resources = district.calculate_yields()
             total_resources_per_turn += resources
@@ -163,21 +179,36 @@ class City:
                 self.housing += 5
             if district.district_type == district_types[6]:
                 self.housing += 2
-                has_aqueduct = True
             if district.district_type == district_types[7]:
                 if city_center_buildings[2] in district.buildings:
                     self.housing += 2
                 if city_center_buildings[4] in district.buildings:
                     self.housing += 4
-        if self.housing - self.population == 1:
-            total_city_resources_per_turn.food_per_turn_count *= 0.75
-        elif self.housing - self.population == 0:
-            total_city_resources_per_turn.food_per_turn_count *= 0.5
-        elif self.housing - self.population < 0:
-            total_city_resources_per_turn.food_per_turn_count *= 0.15
-        if has_aqueduct:
-            total_city_resources_per_turn.food_per_turn_count *= 1.25
         return total_resources_per_turn, total_city_resources_per_turn
 
+    def calculate_yields_tiles(self):
+        total_resources_per_turn = Resources.ResourcesPerTurn(0, 0, 0)
+        total_city_resources_per_turn = Resources.CityResourcesPerTurn(0, 0)
+        for tile in self.tiles:
+            total_resources_per_turn += tile.resources
+            total_city_resources_per_turn += tile.city_resources
+        return total_resources_per_turn, total_city_resources_per_turn
 
+    def end_turn_update(self):
+        self.resources_per_turn, self.city_resources_per_turn = self.calculate_yields_tiles()
+        temp_resources_per_turn, temp_city_resources_per_turn = self.calculate_yields_districts()
+        self.resources_per_turn += temp_resources_per_turn
+        self.city_resources_per_turn += temp_city_resources_per_turn
+
+        if self.housing - self.population == 1:
+            self.city_resources_per_turn.food_per_turn_count *= 0.75
+        elif self.housing - self.population == 0:
+            self.city_resources_per_turn.food_per_turn_count *= 0.5
+        elif self.housing - self.population < 0:
+            self.city_resources_per_turn.food_per_turn_count *= 0.15
+        if district_types[6] in self.districts:
+            self.city_resources_per_turn.food_per_turn_count *= 1.25
+
+        self.city_resources.food_count += self.city_resources_per_turn.food_per_turn_count
+        self.city_resources_per_turn.production_per_turn_count += self.city_resources_per_turn.production_per_turn_count
 
