ffb92a3d37e8b128dd1f72296ac5501bf6f8c393
Vasile VORNICESCU <vasile.vornicescu@stud.acs.upb.ro>
2024-12-08 12:35:52 +0200

    Vertex shader bug fix

--- Shaders/vert.glsl
+++ Shaders/vert.glsl
@@ -16,6 +16,13 @@ layout (location = 2) uniform sampler2D uResourcesTexture; // 2
 
 uniform float resourceId;
 
+uniform float size_x;
+uniform float size_y;
+uniform float len_x;
+uniform float len_y;
+uniform float R;
+uniform float dR;
+
 out vec3 Normal;
 out vec3 FragPos;
 out vec3 Color;
@@ -29,34 +36,27 @@ void main() {
         ivec2 pos = ivec2(gl_InstanceID, resourceId);
         float extract = texelFetch(uResourcesTexture, pos, 0).r;
         float h = texelFetch(uResourcesTexture, pos, 0).g;
-        id = int(extract);
+        id = round(extract);
 
         // find real position
-        int id_x = int(id) / 40;
-        int id_y = int(mod(int(id), 40));
-
-        float R = 1;
-        float dR = 0.3;
-        float len_x = R * cos(0.523599);
-        float len_y = R * sin(0.523599);
+        float id_x = floor(id / size_y);
+        float id_y = id - size_y * id_x;
 
         float x_offset = 0;
         if ((int(id_y) & 1) != 0)
             x_offset = len_x + 0.5 * dR;
-        float x_pos = x_offset + (2 * len_x + dR) * id_x;
-        float y_pos = (len_y + R + dR * sqrt(3.0) / 2.0) * id_y;
-        real_pos.x += x_pos;
-        real_pos.z += y_pos;
+        real_pos.x += x_offset + (2 * len_x + dR) * id_x;
+        real_pos.z += (len_y + R + dR * sqrt(3.0) / 2.0) * id_y;
         real_pos.y += h;
 
         gl_Position = projection * view * real_pos;
     } else {
-        id = aData.y;
+        id = round(aData.y);
         gl_Position = projection * view * model * vec4(aPos, 1.0);
     }
 
     if (id >= 0) {
-        visibility = texelFetch(uVisibilityTexture, int(id), 0).r;
+        visibility = texelFetch(uVisibilityTexture, int(round(id)), 0).r;
     } else {
         visibility = 1.0;
     }
